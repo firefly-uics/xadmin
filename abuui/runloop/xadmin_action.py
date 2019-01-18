cd05a77d3e2c183c4488380d975f7965f6782d2d
bqw_5189 <bqw_5189@163.com>
2019-01-18 16:55:50 +0800

    add mysql

--- abuui/runloop/xadmin_action.py
+++ abuui/runloop/xadmin_action.py
@@ -10,38 +10,44 @@ class MyAction(BaseActionView):
     model_perm = 'change'
 
     def do_action(self, queryset):
-        """
-            8.1.4 对多支股票进行择时
-            :return:
-        """
-        sell_factor1 = {'xd': 120, 'class': AbuFactorSellBreak}
-        sell_factor2 = {'stop_loss_n': 0.5, 'stop_win_n': 3.0, 'class': AbuFactorAtrNStop}
-        sell_factor3 = {'class': AbuFactorPreAtrNStop, 'pre_atr_n': 1.0}
-        sell_factor4 = {'class': AbuFactorCloseAtrNStop, 'close_atr_n': 1.5}
-        sell_factors = [sell_factor1, sell_factor2, sell_factor3, sell_factor4]
-        benchmark = AbuBenchmark()
-        buy_factors = [{'xd': 60, 'class': AbuFactorBuyBreak},
-                       {'xd': 42, 'class': AbuFactorBuyBreak}]
-
-        choice_symbols = ['usTSLA', 'usNOAH']
-        capital = AbuCapital(1000000, benchmark)
-        orders_pd, action_pd, all_fit_symbols_cnt = ABuPickTimeExecute.do_symbols_with_same_factors(choice_symbols,
-                                                                                                    benchmark, buy_factors,
-                                                                                                    sell_factors, capital,
-                                                                                                    show=False)
-
-        metrics = AbuMetricsBase(orders_pd, action_pd, capital, benchmark)
-        metrics.fit_metrics()
-
-        str = ''
-
-        str += '买入后卖出的交易数量:%s' % metrics.order_has_ret.shape[0]
-        str += '胜率:%f%' % metrics.win_rate * 100
-
-        print(str)
 
         for obj in queryset:
-            # 其实我们做的只有这一部分 ********
             obj.status = 'run...'
             obj.save()
+
+            """
+                        8.1.4 对多支股票进行择时
+                        :return:
+                    """
+            sell_factor1 = {'xd': 120, 'class': AbuFactorSellBreak}
+            sell_factor2 = {'stop_loss_n': 0.5, 'stop_win_n': 3.0, 'class': AbuFactorAtrNStop}
+            sell_factor3 = {'class': AbuFactorPreAtrNStop, 'pre_atr_n': 1.0}
+            sell_factor4 = {'class': AbuFactorCloseAtrNStop, 'close_atr_n': 1.5}
+            sell_factors = [sell_factor1, sell_factor2, sell_factor3, sell_factor4]
+            benchmark = AbuBenchmark()
+            buy_factors = [{'xd': 60, 'class': AbuFactorBuyBreak},
+                           {'xd': 42, 'class': AbuFactorBuyBreak}]
+
+            choice_symbols = ['usTSLA', 'usNOAH']
+            capital = AbuCapital(1000000, benchmark)
+            orders_pd, action_pd, all_fit_symbols_cnt = ABuPickTimeExecute.do_symbols_with_same_factors(choice_symbols,
+                                                                                                        benchmark,
+                                                                                                        buy_factors,
+                                                                                                        sell_factors,
+                                                                                                        capital,
+                                                                                                        show=False)
+
+            metrics = AbuMetricsBase(orders_pd, action_pd, capital, benchmark)
+            metrics.fit_metrics()
+
+            str = ''
+
+            str += '买入后卖出的交易数量:%s \n' % metrics.order_has_ret.shape[0]
+            str += '胜率:%f \n' % (metrics.win_rate * 100)
+
+            # 其实我们做的只有这一部分 ********
+            obj.status = 'done'
+            obj.description = str
+            obj.save()
         # return HttpResponse('{"status": "success", "msg": "error"}', content_type='application/json')
+

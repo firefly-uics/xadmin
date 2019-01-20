b712db3f5f93d5f4455281b402729f431106d6da
bqw_5189 <bqw_5189@163.com>
2019-01-20 13:36:59 +0800

    add management init

--- abuui/management.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_syncdb
-from base import models as base_app
-from base.models import *
-
-def setup_base(sender, **kwargs):
-    Stock(co_name="锐捷网络", symbol="002735", market="SZ").save()
-
-    post_syncdb.connect(setup_base, sender=base_app)

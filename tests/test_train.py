af9178e64b96cf851309b63251bf90dbd111a1d6
bqw_5189 <bqw_5189@163.com>
2019-02-13 14:28:47 +0800

    test

--- tests/test_train.py
+++ tests/test_train.py
@@ -17,15 +17,17 @@ def train():
     sell_factor4 = {'class': AbuFactorCloseAtrNStop, 'close_atr_n': 1.5}
     sell_factors = [sell_factor1, sell_factor2, sell_factor3, sell_factor4]
 
-    benchmark_kl_pd = ABuSymbolPd.make_kl_df('002396', data_mode=EMarketDataSplitMode.E_DATA_SPLIT_SE,
+    symbols = ['002396', '002713']
 
-                                             start='2018-12-01')
+    # benchmark_kl_pd = ABuSymbolPd.make_kl_df(symbols, data_mode=EMarketDataSplitMode.E_DATA_SPLIT_SE,
+    #
+    #                                          start='2018-10-01')
 
     # benchmark_kl_pd = pd.read_csv('/Users/bailijiang/abu/data/csv/sz002396_20160104_20190203')
 
-    print(benchmark_kl_pd)
+    # print(benchmark_kl_pd)
 
-    benchmark = AbuBenchmark(benchmark_kl_pd=benchmark_kl_pd)
+    benchmark = AbuBenchmark(start='2018-10-01')
 
     buy = {'xd': 7, 'class': AbuFactorBuyBreak}
     position = {'position': AbuKellyPosition}
@@ -36,8 +38,8 @@ def train():
 
     buy_factors = [buy]
 
-    capital = AbuCapital(100000, benchmark)
-    orders_pd, action_pd, all_fit_symbols_cnt = ABuPickTimeExecute.do_symbols_with_same_factors(['002396'],
+    capital = AbuCapital(50000, benchmark)
+    orders_pd, action_pd, all_fit_symbols_cnt = ABuPickTimeExecute.do_symbols_with_same_factors(symbols,
                                                                                                 benchmark,
                                                                                                 buy_factors,
                                                                                                 sell_factors,

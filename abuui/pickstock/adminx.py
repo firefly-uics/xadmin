0d6e02cbc8f51ed2e18b0cc72d3b82ba41184624
bqw_5189 <bqw_5189@163.com>
2019-02-14 16:53:00 +0800

    add shitf dist

--- abuui/pickstock/adminx.py
+++ abuui/pickstock/adminx.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.utils.translation import ugettext as _
 
 import xadmin
-from .models import PriceMinMaxPickStock
+from .models import PriceMinMaxPickStock, ShiftDistancePickStock
 
 ACTION_NAME = {
     'add': _('Can add %s'),
@@ -17,6 +17,25 @@ ACTION_NAME = {
 @xadmin.sites.register(PriceMinMaxPickStock)
 class PriceMinMaxPickStockAdmin(object):
     list_display = ("name", "xd", "reversed", "price_min", "price_max")
+
+    list_display_links = ("name",)
+
+    search_fields = ["name"]
+
+    list_filter = [
+        "name"
+    ]
+
+    list_quick_filter = [{"field": "name", "limit": 10}]
+
+    search_fields = ["name"]
+
+    reversion_enable = True
+
+
+@xadmin.sites.register(ShiftDistancePickStock)
+class ShiftDistancePickStockAdmin(object):
+    list_display = ("name", "threshold_sd", "reversed", "min", "max")
 
     list_display_links = ("name",)
 
--- abuui/pickstock/models.py
+++ abuui/pickstock/models.py
@@ -53,7 +53,7 @@ class PriceMinMaxPickStock(PickStock):
       2. 交易目标最大价格 < 最大价格阀值
     """
     price_min = models.FloatField(verbose_name=u"最小:", default=15)
-    price_min_ck = models.BooleanField(verbose_name=u"使用最小阀值", choices=PRICE_MIN_CK_SELECT,)
+    price_min_ck = models.BooleanField(verbose_name=u"使用最小阀值", choices=PRICE_MIN_CK_SELECT, )
     price_max = models.FloatField(verbose_name=u"最大:", default=50)
     price_max_ck = models.BooleanField(verbose_name=u"使用最大阀值", choices=PRICE_MIN_CK_SELECT, )
 
@@ -76,3 +76,36 @@ class PriceMinMaxPickStock(PickStock):
 
     def delegate_class(self):
         return 'AbuPickStockPriceMinMax'
+
+
+@python_2_unicode_compatible
+class ShiftDistancePickStock(PickStock):
+    """
+    位移路程比选股因子策略：
+      将交易目标走势每月计算价格位移路程比，根据比值进行选股，选取波动程度不能太大，也不太小的目标：
+      1. 定义位移路程比大于参数阀值的月份为大波动月
+      2. 一年中大波动月数量 < 最大波动月个数
+      3. 一年中大波动月数量 > 最小波动月个数
+    """
+    threshold_sd = models.FloatField(verbose_name=u"阀值:", default=2.0, validators=[MinValueValidator(1), MaxValueValidator(6), ], )
+    min = models.IntegerField(verbose_name=u"范围-小:", default=1,
+                              validators=[MinValueValidator(1), MaxValueValidator(10), ], )
+    max = models.IntegerField(verbose_name=u"范围-大:", default=4,
+                              validators=[MinValueValidator(1), MaxValueValidator(10), ], )
+
+    class Meta:
+        verbose_name = u"位移路程"
+        verbose_name_plural = verbose_name
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.class_name = "{'threshold_sd': %f,'reversed': %s,'threshold_max_cnt': %d,'threshold_min_cnt': %d, 'class': %s}" % (
+            self.threshold_sd, self.reversed, self.max, self.min, self.delegate_class())
+        self.pick_stock_name = self._meta.verbose_name
+        super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return '策略名称: %s, 位移路程选股大波动:%s 最大:%s 最小:%s ,反转:%s ' % (
+            self.name, self.threshold_sd, self.max, self.min, self.reversed)
+
+    def delegate_class(self):
+        return 'AbuPickStockShiftDistance'
